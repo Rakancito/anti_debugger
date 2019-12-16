50061ddfa7d59b19da1241bf9a0a51685b523321
Robert <55710848+Rakancito@users.noreply.github.com>
2019-12-15 18:00:42 -0600

    Update StdAfx.h

--- StdAfx.h
+++ StdAfx.h
@@ -1,3 +1,19 @@
+// Search:
+
+#include "GameType.h"
+extern DWORD __DEFAULT_CODE_PAGE__;
+#define APP_NAME	"Metin 2"
+
+// Add after or before:
+
+#define APP_PROCESS "metin2client.exe"
+
+//Search:
+
+void initudp();
+
+//Add before:
+
 void AntiDebugScanProcess();
 bool vKillProcess(const char *filename);
 void initThreadAntiDebug();
